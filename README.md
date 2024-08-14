6f46702e6f47126e481407bb41c163f14e064d0c
Štěpán Záliš <stepan.zalis@applifting.cz>
2024-08-14 18:24:02 +0200

    Initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# aktin_product_viewer
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/aktin_product_viewer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aktin_product_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
